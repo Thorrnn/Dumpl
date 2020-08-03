a454fd751d0b90ea4dd0cd2592762d72d57fc5af
Thorrnn <dmitry.gladchenko@gmail.com>
2020-08-03 10:24:23 +0300

    Create TSingleTone, Registry, BlogApp

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+    return[
+        'prod_name' => 'Content Analysis',
+        'pagination' => 3,
+        'smpt_port' => '2525',
+        'smpt_protocol' => 'ssl',
+        'smpt_login' => 'smpt_login',
+        'smpt_password' => '12345678',
+    ];
