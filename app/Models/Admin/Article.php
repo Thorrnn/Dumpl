1f4b4ac27aba28030d0b0431f79bdbd613361fdf
Thorrnn <dmitry.gladchenko@gmail.com>
2021-03-23 23:09:52 +0200

    stat_article

--- app/Models/Admin/Article.php
+++ app/Models/Admin/Article.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = [
-        'title', 'body', 'annotation', 'info', 'annotation', 'status', 'author_id', 'fieldsArticles'
+        'title', 'body', 'annotation', 'info', 'annotation', 'status', 'author_id', 'fieldsArticles', 'id'
     ];
     public function user()
     {
