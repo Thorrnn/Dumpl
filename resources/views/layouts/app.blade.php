71e4c18f2ddf39686f0c4a20b0f9bf5a20c4922e
Thorrnn <dmitry.gladchenko@gmail.com>
2021-04-26 23:09:09 +0300

    fix app_blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,68 +24,69 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm ">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('welcome')}}" style="text-shadow: 1px 1px 0px #000;  font-size:30px">Content Analysis</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('welcome')}}" style="text-shadow: 1px 1px 0px #000;  font-size:30px">Content
+            Analysis</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('welcome')}}">Головна <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('blog.user.tests.index')}}">Тести</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('blog.user.surveys.index')}}">Опитування</a>
+                </li>
+                @if (Route::has('login'))
+                    @auth
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav">
-                    <li class="nav-item mx-1">
-                        <a href="{{route('welcome')}} "style="font-size:20px; color:black">Головна</a>
-                    </li>
-                    <li class="nav-item mx-1">
-                        <a href="{{route('blog.user.tests.index')}}" style="font-size:20px; color:black">Тести</a>
-                    </li>
-                    <li class="nav-item mx-1">
-                        <a href="{{route('blog.user.surveys.index')}}"style="font-size:20px; color:black">Опитування</a>
-                    </li>
-
-                    @if (Route::has('login'))
-                        @auth
-
-                            @if((Auth::user()->role =='user'))
-                                <li class="nav-item mx-1">
-                                    <a href="{{route('index')}}" style="font-size:20px; color:black">Кабінет User</a>
-                                </li>
-                            @elseif(Auth::user()->role =='admin')
-                                <li class="nav-item mx-1">
-                                    <a href="{{route('blog.admin.articles.index')}}" style="font-size:20px; color:black">Панель адміністратора</a>
-                                </li>
-                            @endif
-
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-            </div>
-            <div class="flex-center collapse navbar-collapse">
-
-                <ul class="navbar-nav">
+                        @if((Auth::user()->role =='user'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('index')}}" style="color:black">Кабінет User</a>
+                            </li>
+                        @elseif(Auth::user()->role =='admin')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('blog.admin.articles.index')}}" style="color:black">Панель
+                                    адміністратора</a>
+                            </li>
+                        @endif
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size:20px">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false" style="font-size:20px">
                             {{ Auth::user()->name }}
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item " style="font-size:20px; color:black" href="{{ route('logout') }}"
+                        <div class="dropdown-menu" style="right: 0;left: auto;"
+                             aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" style="color:black" href="{{ route('logout') }}"
                                onclick="event.preventDefault()
                                 document.getElementById('logout-form').submit();">
                                 Выйти
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method = "POST" style="display: none";>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none" ;>
                                 @csrf
                             </form>
                         </div>
                     </li>
-                    @else<a href="{{ route('login') }}" style="font-size:20px; color:black; text-decoration: none" class="ml-2">Увійти</a>
-                    @if(Route::has('register')) <a href="{{route('register')}}" style="font-size:20px; color:black; text-decoration: none" class="ml-2">Реєстрація</a>@endif
-                    @endauth
-                    @endif
                 </ul>
             </div>
+            @else
+                <div class="form-inline my-2 my-lg-0">
+                    <a class="nav-link" href="{{ route('login') }}" style="color:black; text-decoration: none"
+                    >Увійти</a>
+                    @if(Route::has('register')) <a class="nav-link" href="{{route('register')}}"
+                                                   style="color:black; text-decoration: none"
+                    >Реєстрація</a>@endif
+                    @endauth
+                    @endif
+                </div>
         </div>
     </nav>
 
